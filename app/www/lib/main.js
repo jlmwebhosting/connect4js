d09d98543729b9691f6f6a660f7ae3b7708ad2b2
John C. Bland II <john.bland@webfilings.com>
2012-12-15 15:50:25 -0700

    Initial require config

--- app/www/lib/main.js
+++ app/www/lib/main.js
@@ -1,7 +1,37 @@
-require(["jquery"],
-		function($) {
-	    $(function() {
-	        //do stuff here
+'use strict';
+
+require.config = {
+    paths: {
+    	'domRead': 'require/domReady',
+        'text':'require/require-text',
+        'jquery': 'jquery/jquery',
+        'angular': 'angular/angular.min',
+        'angular-resource': 'angular/angular-resource',
+        'angular-cookies': 'angular/angular-cookies',
+        'underscore': 'underscore/underscore-min',
+        'socket.io': '/socket.io/socket.io'
+    },
+    shim: {
+        'jquery': {
+            deps: [],
+            exports: '$'
+        },
+        'angular': {
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        'angular-resource': ['angular'],
+        'angular-cookies': ['angular'],
+        'underscore': {
+            exports: '_'
+        }
+    }
+};
+
+require(["domReady", "jquery", "angular", "socket.io"],
+	function(domReady, $, angular){
+	   	domReady(function(){
+	        console.log(_);
 	    });
 	}
 );
