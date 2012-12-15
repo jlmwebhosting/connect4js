2f6616133674d5b67e7bb4a90db32468f1103b44
Rob Richardson <robrich@robrich.org>
2012-12-15 15:35:06 -0700

    The server, client socket.io content, and dummy html elements to play with it

--- app/www/lib/main.js
+++ app/www/lib/main.js
@@ -1,7 +1,24 @@
-require(["jquery"],
-		function($) {
-	    $(function() {
-	        //do stuff here
-	    });
+require(["socket.io","jquery","domReady"],
+	function(io, $) {
+
+		var message = $("#message");
+
+		var socket = io.connect('/');
+		socket.on("waiting", function (data) {
+			var msg = (data && data.message) || "waiting ...";
+			message.text(msg);
+		});
+		socket.on("player_move", function (data) {
+			message.text("player move: "+JSON.stringify(data));
+		});
+		var move = function () {
+			socket.emit("move", {message:'The message'});
+		};
+		$("#move").click(function (e) {
+			if (e.button === 0) {
+				move();
+			}
+		});
+
 	}
 );
