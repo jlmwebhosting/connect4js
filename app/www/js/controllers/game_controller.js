9fa55fdbbf53e99255c0ca6c8e41b6c88334449c
John C. Bland II <john.bland@webfilings.com>
2012-12-15 18:35:02 -0700

    Added player turns

--- app/www/js/controllers/game_controller.js
+++ app/www/js/controllers/game_controller.js
@@ -2,14 +2,17 @@ define(['angular', "socket", "jquery"],
 	function(angular, socket, $){
 		var GameController = function($scope){
 			var gameBoard = $("#gameBoard");
+			var yourTurn = true;
 
 			$scope.message = "Loading ...";
 
 			$scope.drop = function(position){
-				console.log("dropped", position);
+				if(!yourTurn) return;
 				var data = {type: "normal", position: position};
 				socket.move(data);
 				processMove("you", data);
+				yourTurn = false;
+				$scope.message = "Waiting on opponent to play";
 			}
 
 			socket.callbacks.message = function (msg) {
@@ -17,36 +20,18 @@ define(['angular', "socket", "jquery"],
 				$scope.$apply();
 			};
 			socket.callbacks.player_move = function (data) {
-				console.log(data);
-				//move handler; inbound
-				//$scope.message = "player move: "+JSON.stringify(data);
 				processMove("them", data);
+				yourTurn = true;
+				$scope.message = "Your turn";
+				$scope.$apply();
 			};
 
 			var processMove = function(user, data){
 				var type = data.type;
 				var position = data.position;
 				var td = gameBoard.find("td:nth-child(" + position + ")").not(".used");
-				switch(type){
-					case "normal":
-						console.log(user, "Move: normal", position);
-						td.last().addClass("used");
-						td.last().addClass(user);
-						console.log(td);
-					break;
-					case "explode":
-						console.log(user, "Move: explode");
-					break;
-					case "invertColumn":
-						console.log(user, "Move: invert column");
-					break;
-					case "invertRow":
-						console.log(user, "Move: invert row");
-					break;
-					default:
-						console.log(user, "invalid move");
-					break;
-				}
+				td.last().addClass("used");
+				td.last().addClass(user);
 			}
 		}
 
